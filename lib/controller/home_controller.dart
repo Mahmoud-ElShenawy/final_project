dfac795699f9186443ef1234fc6391076ac41986
Mahmoud El Shenawy <m.elshenawy@intcore.net>
2022-12-21 20:06:50 +0200

    Working on Generic APIS

---
+++ lib/controller/home_controller.dart
@@ -0,0 +1,9 @@
+import 'package:dio/dio.dart';
+import 'package:final_project/network/network_helper.dart';
+
+class HomeController {
+  final Dio _dio = Dio();
+  String url = 'https://fakestoreapi.com/products';
+
+
+}
---
+++ lib/network/api_constants.dart
@@ -0,0 +1,9 @@
+import 'package:final_project/config/app_constants.dart';
+
+class ApiConstants{
+  static String BASE_URL = AppConstants.URL;
+
+  static String products = BASE_URL+ 'products';
+
+
+}
